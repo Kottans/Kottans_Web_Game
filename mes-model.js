3003364f83449f2b369925970bd7d124ce85a4b2
m-vv <m-vv@users.noreply.github.com>
2017-03-03 16:38:58 +0200

    Feathers service mess with mongodb integration

---
+++ mes-model.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// mes-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+// this is just for text MognoDB with mongoose
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const mesSchema = new Schema({
+  text: { type: String, required: true },
+  createdAt: { type: Date, 'default': Date.now },
+  updatedAt: { type: Date, 'default': Date.now }
+});
+
+const mesModel = mongoose.model('mes', mesSchema);
+
+module.exports = mesModel;
