8fc052d8b3f7c5eebb27f4098a936e29eb1d9ffc
amashoshyna <a.mashoshyna@gmail.com>
2017-04-22 00:40:14 +0300

    secondary route for login-form component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainhallComponent } from './components/mainhall/mainhall.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { LoginFormComponent } from './components/login-form/login-form.component'
 
 const appRoutes: Routes = [
     { path: '', component: MainhallComponent },
-    { path: 'profile', component: UserProfileComponent }
+    { path: 'profile', component: UserProfileComponent },
+    { path: 'login', component: LoginFormComponent, outlet: 'popup' }
 
 ]
 
