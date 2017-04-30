832ceaa7915d7399ae4b37cf19b90a5327898401
amashoshyna <a.mashoshyna@gmail.com>
2017-04-30 18:51:40 +0300

    remove "unique" requirement on text messages

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,14 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-
 const MessageSchema = new Schema({
-  text: {type: String, required: true, unique: true},
+  text: { type: String, required: true, unique: false },
   // password: {type: String, required: true },
-  createdAt: {type: Date, 'default': Date.now},
-  updatedAt: {type: Date, 'default': Date.now}
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  title: { type: String, default: 'Title'}
 });
 
-var MessageModel = mongoose.model('Message', MessageSchema);
+var MessageModel = mongoose.model("Message", MessageSchema);
 
 module.exports = MessageModel;
