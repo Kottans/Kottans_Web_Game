f71e3b3121c65d5de3d50698c90cb1a82066d212
amashoshyna <a.mashoshyna@gmail.com>
2017-05-03 01:30:36 +0300

    make login-form use auth service

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiRealtimeService } from '../../services/api-realtime.service';
-
-// import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import {
     AbstractControl, FormArray, FormBuilder,
     FormControl, FormGroup, Validators
@@ -18,13 +17,15 @@ export class LoginFormComponent implements OnInit {
     private _feathers;
     loginData: FormGroup;
     error = '';
+    user = '';
 
     constructor(
-        // private authService: AuthService, 
+        private authService: AuthService,
         private feathers: ApiRealtimeService,
         public router: Router) {
-        // this._authService = authService;
+        this._authService = authService;
         this._feathers = feathers;
+        console.log(this._feathers._feathers.get())
 
         this.loginData = new FormGroup({
             email: new FormControl(),
@@ -32,25 +33,24 @@ export class LoginFormComponent implements OnInit {
         });
     }
 
-
-
     login(email: string, password: string) {
         this.error = '';
         if (!email || !password) {
             return;
         }
 
-        this.feathers.authenticate({
-            strategy: 'local',
+        this._authService.logIn(
             email,
             password
-        })
+        )
             .then((result) => {
-                console.log(result)
                 this.router.navigate(['/']);
             }, error => {
                 this.error = 'Authentication failed';
                 console.log(error)
+            })
+            .then(() => {
+                this._feathers.service('users').get();
             })
             .catch(err => {
                 console.error(err);
